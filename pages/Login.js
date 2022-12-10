901ef4e3c1b4a403827cd7983e14003f6eacec36
rakshitha31 <rakshitha31@gmail.com>
2022-12-10 16:07:36 -0700

    update:login page UI updated

--- pages/Login.js
+++ pages/Login.js
@@ -1,6 +1,6 @@
 export default function Login() {
         return (
-            <section class="bg-gray-50 dark:bg-gray-900">
+            <section class="bg-slate-200 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -28,7 +28,7 @@ export default function Login() {
                       </div>
                       <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <button type="submit" class="w-full text-black bg-violet-300 hover:bg-violet-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                 
               </form>
           </div>
--- pages/dashboard/about.js
+++ pages/dashboard/about.js
@@ -1,8 +1,12 @@
+import Layout from "../../components/layout";
+
 export default function About() {
     return (
       <div className='flex min-h-screen flex-col justify-center items-center'>
+        <Layout/>
         <h1 className='text-4xl mb-5 font-bold'>About</h1>
         <span className='text-7xl'>💬</span>
+
       </div>
     );
   }
